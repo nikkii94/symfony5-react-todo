aa7a3d41d34b8670027cb614718dba15b3eac5b8
nikkii94 <k.nikolett17@gmail.com>
2020-01-03 23:08:06 +0100

    MaterialUI added

--- assets/js/components/TodoTable.js
+++ assets/js/components/TodoTable.js
@@ -1,15 +1,40 @@
 import React, {useContext} from 'react';
 import {TodoContext} from "../contexts/TodoContext";
+import Table from "@material-ui/core/Table";
+import {TableHead, TableRow, TableCell, TableBody} from "@material-ui/core";
+import IconButton from "@material-ui/core/IconButton";
+import EditIcon from "@material-ui/icons/Edit";
+import DeleteIcon from "@material-ui/icons/Delete";
 
 function TodoTable() {
     const context = useContext(TodoContext);
 
     return (
-        <div>
-            {context.todos.map(todo => (
-                <div>{todo.name}</div>
-            ))}
-        </div>
+        <Table>
+            <TableHead>
+                <TableRow>
+                    <TableCell>Task</TableCell>
+                    <TableCell align="right">Actions</TableCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                {context.todos.map(todo => (
+                    <TableRow>
+                        <TableCell>
+                            {todo.name}
+                        </TableCell>
+                        <TableCell align="right">
+                            <IconButton>
+                                <EditIcon/>
+                            </IconButton>
+                            <IconButton>
+                                <DeleteIcon/>
+                            </IconButton>
+                        </TableCell>
+                    </TableRow>
+                ))}
+            </TableBody>
+        </Table>
     );
 }
 
